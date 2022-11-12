905a76a486e8b8772f49f7170f99ef3dadcc9baa
Amelia18354 <79320016+Amelia18354@users.noreply.github.com>
2022-11-12 19:17:33 +0000

    Added Fed register parser, but missing some fields

--- src/main/java/ie/tcd/group7/CreateIndex.java
+++ src/main/java/ie/tcd/group7/CreateIndex.java
@@ -1,5 +1,7 @@
 package ie.tcd.group7;
+
 import ie.tcd.group7.FT;
+import ie.tcd.group7.FR;
 
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
@@ -35,17 +37,13 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+public class CreateIndex {
 
-public class CreateIndex
-{
-    
     // Directory where the search index will be saved
     private static String INDEX_DIRECTORY = "index";
     private static String CRAN_DATA = "data/Assignment Two/Assignment Two/ft/ft911/ft911_1";
 
-    public static int createIndex(Analyzer analyzer) throws IOException
-    {
-
+    public static int createIndex(Analyzer analyzer) throws IOException {
 
         Directory directory = FSDirectory.open(Paths.get(INDEX_DIRECTORY));
 
@@ -60,134 +58,115 @@ public class CreateIndex
             BufferedReader cranReader = new BufferedReader(new FileReader(CRAN_DATA));
             String currLine = cranReader.readLine();
             int docNumbers = 0;
-             /*
-            while(currLine != null) {
-                String title = "";
-                String author = "";
-                String bib = "";
-                String words = "";
-                Document doc = new Document();
+            /*
+             * while(currLine != null) {
+             * String title = "";
+             * String author = "";
+             * String bib = "";
+             * String words = "";
+             * Document doc = new Document();
+             * 
+             * // checking for new document
+             * if (currLine.startsWith(".I")) {
+             * // adding doc id to document
+             * String id_val = currLine.substring(3).trim();
+             * doc.add(new StringField("id", id_val, Field.Store.YES));
+             * currLine = cranReader.readLine();
+             * String currAtr = ""; // used to keep track of what doc element is being read
+             * while (currLine != null) {
+             * if (currLine.startsWith(".T") || currLine.startsWith(".A") ||
+             * currLine.startsWith(".B") || currLine.startsWith(".W")) {
+             * currAtr = currLine.substring(0,2);
+             * currLine = cranReader.readLine();
+             * } else if (currLine.startsWith(".I")) { // end of doc
+             * break;
+             * }
+             * // add space as there may be two words joined otherwise
+             * if (!currLine.substring(0,1).equals(" ")) {
+             * currLine = " " + currLine;
+             * }
+             * if (currAtr.equals(".T")) {
+             * title = title + currLine;
+             * } else if (currAtr.equals(".A")) {
+             * author = author + currLine;
+             * } else if (currAtr.equals(".B")) {
+             * bib = bib + currLine;
+             * } else if (currAtr.equals(".W")) {
+             * words = words + currLine;
+             * }
+             * currLine = cranReader.readLine();
+             * }
+             * doc.add(new TextField("title", title.trim(), Field.Store.YES));
+             * doc.add(new TextField("author", author.trim(), Field.Store.YES));
+             * doc.add(new TextField("bib", bib.trim(), Field.Store.YES));
+             * doc.add(new TextField("content", words.trim(), Field.Store.YES));
+             * iwriter.addDocument(doc);
+             * }
+             * 
+             * docNumbers++;
+             * }
+             * 
+             * 
+             * Path path = Paths.get("data/Assignment Two/Assignment Two/ft/ft911/ft911_1");
+             * byte[] fileBytes = Files.readAllBytes(path);
+             * String fileString = new String(fileBytes, StandardCharsets.ISO_8859_1);
+             * 
+             * 
+             * 
+             * 
+             * // final File FT_DIR = new File("data/Assignment Two/Assignment Two/ft");
+             * //String get_path = FT_DIR.getAbsolutePath();
+             * 
+             * //public static void access_directory(String get_path){
+             * // String file4;
+             * //}
+             * 
+             * access_directory(FT_DIR.getAbsolutePath());
+             * //get abs path of ft folder
+             * //File get_path = new File(FT_DIR.getAbsolutePath());
+             * //returns and stores as list the list of files in that folder
+             * File[] file_list = get_path.listFiles();
+             * 
+             * while(file_list != null){
+             * for(File file : file_list){
+             * if(file.isDirectory()){
+             * 
+             * }
+             * }
+             * }
+             * 
+             * // currLine = cranReader.readLine();
+             * 
+             * 
+             * */
+             
 
-                // checking for new document
-                if (currLine.startsWith(".I")) {
-                    // adding doc id to document
-                    String id_val = currLine.substring(3).trim();
-                    doc.add(new StringField("id", id_val, Field.Store.YES));
-                    currLine = cranReader.readLine();
-                    String currAtr = ""; // used to keep track of what doc element is being read
-                    while (currLine != null) {
-                        if (currLine.startsWith(".T") || currLine.startsWith(".A") || currLine.startsWith(".B") || currLine.startsWith(".W")) {
-                            currAtr = currLine.substring(0,2);
-                            currLine = cranReader.readLine();
-                        } else if (currLine.startsWith(".I")) { // end of doc
-                            break;
-                        }
-                        // add space as there may be two words joined otherwise
-                        if (!currLine.substring(0,1).equals(" ")) {
-                            currLine = " " + currLine;
-                        }
-                        if (currAtr.equals(".T")) {
-                            title = title + currLine;
-                        } else if (currAtr.equals(".A")) {
-                            author = author + currLine;
-                        } else if (currAtr.equals(".B")) {
-                            bib = bib + currLine;
-                        } else if (currAtr.equals(".W")) {
-                            words = words + currLine;
-                        }
-                        currLine = cranReader.readLine();
-                    }
-                    doc.add(new TextField("title", title.trim(), Field.Store.YES));
-                    doc.add(new TextField("author", author.trim(), Field.Store.YES));
-                    doc.add(new TextField("bib", bib.trim(), Field.Store.YES));
-                    doc.add(new TextField("content", words.trim(), Field.Store.YES));
-                    iwriter.addDocument(doc);
-                }
-                
-                docNumbers++;
-            }
-            
-            
-            Path path = Paths.get("data/Assignment Two/Assignment Two/ft/ft911/ft911_1");
-            byte[] fileBytes = Files.readAllBytes(path);
-            String fileString = new String(fileBytes, StandardCharsets.ISO_8859_1);
+// ----------------------Parsing Financial Times data----------------------------
 
-
-
-
-           // final File FT_DIR = new File("data/Assignment Two/Assignment Two/ft");
-           //String get_path = FT_DIR.getAbsolutePath();
-
-/*    //public static void access_directory(String get_path){
-              //  String file4;
-            //}
-
-            access_directory(FT_DIR.getAbsolutePath());
-            //get abs path of ft folder
-            //File get_path = new File(FT_DIR.getAbsolutePath());
-            //returns and stores as list the list of files in that folder 
-            File[] file_list = get_path.listFiles();
-
-            while(file_list != null){
-                    for(File file : file_list){
-                    if(file.isDirectory()){
-
-                    }
-                }
-            }
-        
-         
-            //convert the file to a string 
-            
-                //using Jsoup parsing package to split into fields by <> tag 
-                org.jsoup.nodes.Document soup = Jsoup.parse(fileString);
-
-                //splits the file at each new DOC tag
-                List<Element> elements = soup.getElementsByTag("DOC");
-                Document document = new Document();
-
-                //for each DOC, split it into its various fields
-                for(Element element : elements){
-
-                //Creates a document with the fields specified to be written to an index
-                String id = element.getElementsByTag("DOCNO").text();
-                document.add(new StringField("id", id, Field.Store.YES));
-
-                String headline = element.getElementsByTag("HEADLINE").text();
-                document.add(new StringField("title", headline, Field.Store.YES));
-
-                String profile = element.getElementsByTag("PROFILE").text();
-                document.add(new TextField("profile", profile, Field.Store.YES));
-
-                String date = element.getElementsByTag("DATE").text();
-                document.add(new TextField("date", date, Field.Store.YES));
-
-                String text = element.getElementsByTag("TEXT").text();
-                document.add(new TextField("text", text, Field.Store.YES));
-
-                String pub = element.getElementsByTag("PUB").text();
-                document.add(new TextField("pub", pub, Field.Store.YES));
-
-                String page = element.getElementsByTag("PAGE").text();
-                document.add(new TextField("page", page, Field.Store.YES));
-
-                */
-
-               // currLine = cranReader.readLine();
-              
-               //FT class to access functions in FT.java
-                FT data = new FT();
-                //parse_file function in FT.java splits the doc into fields. 
-                ArrayList<Document> documents = data.parse_FT();
-                //for loop to parse every occurence of DOC tag, not just one. 
-                for(Document document : documents){
+            // FT class to access functions in FT.java
+            FT data = new FT();
+            // parse_file function in FT.java splits the doc into fields.
+            ArrayList<Document> documents = data.parse_FT();
+            // for loop to parse every occurence of DOC tag, not just one.
+            for (Document document : documents) {
                 iwriter.addDocument(document);
                 docNumbers++;
-                //System.out.println(document);
-                }
-                
+                // System.out.println(document);
+            }
+            
 
-        
+// ----------------------Parsing Federal Register Data----------------------------
+
+            FR FRdata = new FR();
+            // parse_file function in FT.java splits the doc into fields.
+            ArrayList<Document> FRdocuments = FRdata.parse_FR();
+            // for loop to parse every occurence of DOC tag, not just one.
+            for (Document FRdocument : FRdocuments) {
+                iwriter.addDocument(FRdocument);
+                docNumbers++;
+                System.out.println(FRdocument);
+            }
+
             cranReader.close();
             System.out.println("FINISHED: Indexing, total docs added is " + docNumbers);
 
@@ -200,15 +179,15 @@ public class CreateIndex
         return scoringType;
     }
 
-    // asks the user for scoring type and sets the scoring for indexing of docs and queries
-    public static int setScoring(IndexWriterConfig config)
-    {
+    // asks the user for scoring type and sets the scoring for indexing of docs and
+    // queries
+    public static int setScoring(IndexWriterConfig config) {
         Scanner scoringIn = new Scanner(System.in);
         System.out.println("Please select the type of Scoring:\n1. 1 for BM25\n2. 2 for Classic (VSM)\n3. " +
                 "3 for Boolean\n4. 4 for LMDirichlet");
         int scoringType = scoringIn.nextInt();
 
-        switch(scoringType) {
+        switch (scoringType) {
             case 1:
                 config.setSimilarity(new BM25Similarity());
                 System.out.println("Selected BM25 for scoring.");
