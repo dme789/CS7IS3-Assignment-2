411bb255c68e7b6f47a598e7233ebb2a70db03e4
dommeudec <72738163+dommeudec@users.noreply.github.com>
2022-11-15 13:59:54 +0000

    Tidied data folder and added topics

--- src/main/java/ie/tcd/group7/FR.java
+++ src/main/java/ie/tcd/group7/FR.java
@@ -44,7 +44,7 @@ public class FR {
     </DOC>
      */
 
-    private final static File FR_DIR = new File("data/Assignment Two/Assignment Two/fr94");
+    private final static File FR_DIR = new File("data/Assignment Two/Documents/fr94");
     ArrayList<Document> fr_data = new ArrayList();
 
     public ArrayList<Document> parse_FR() throws IOException {
